e5b75f355f5cde7ec9645b6b004a09b285e45b0a
Tayson Le <ltt256@gmail.com>
2021-07-21 17:57:51 +0700

    C0 - Pratice - Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {FormsModule} from '@angular/forms'
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import{WordComponent} from './word/word.component';
+import { BookComponent } from './book/book.component';
+import { UserFormComponent } from './user-form/user-form.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WordComponent,
+    BookComponent,
+    UserFormComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
