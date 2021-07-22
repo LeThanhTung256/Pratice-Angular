3f93c3bdd810fc0a80ddbb0e4e1053ebabec938d
Tayson Le <ltt256@gmail.com>
2021-07-22 17:32:59 +0700

    C2 - update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,17 @@ import { AppComponent } from './app.component';
 import{WordComponent} from './word/word.component';
 import { BookComponent } from './book/book.component';
 import { UserFormComponent } from './user-form/user-form.component';
+import { StructComponent } from './struct/struct.component';
+import { WordsComponent } from './words/words.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WordComponent,
     BookComponent,
-    UserFormComponent
+    UserFormComponent,
+    StructComponent,
+    WordsComponent
   ],
   imports: [
     BrowserModule,
